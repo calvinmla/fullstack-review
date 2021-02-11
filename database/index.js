c8f73345206e4a9a25bb59ad43b99620c64e31ea
Calvin <calvinminhla@gmail.com>
2021-02-10 20:32:15 -0800

    Hopefully completed the save function

--- database/index.js
+++ database/index.js
@@ -26,10 +26,13 @@ let repoSchema = new mongoose.Schema({
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (...repos) => {
+  for (let repo of repos) {
+    repo.save((err, result) => {
+      if (err) throw err;
+      console.log(`Saved ${repo}!)`)
+    })
+  }
 }
 
 module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/repos', function (req, res) {
-  console.log(req.data)
+  // console.log(req.data)
+  let x = getReposByUsername;
+  console.log(x);
+
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
